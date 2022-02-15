391d5f1b6561c170b24669bb49243e14133cb9ad
ArtyDy <arthur.devemy@orange.fr>
2022-02-15 10:41:33 +0100

    Update rftest.py

--- rftest.py
+++ rftest.py
@@ -1,8 +1,20 @@
 #%%
 
-from psychopy import visual
-win=visual.Window()
+import psychopy
+from psychopy import visual, core
 
-rect=visual.Rect(win, 50, 50, )
 
+
+win = psychopy.visual.Window(
+    size=[400, 400],
+    units="pix",
+    fullscr=True,
+    color=[0, 0, 0]
+)
+
+rect=visual.Rect(win, width=50, height=50,fillColor=[0, 0, 0], lineWidth=5, lineColor=[1, 1, 1] )
+rect.draw()
+win.flip()
+core.wait(5)
+win.close()
 # %%

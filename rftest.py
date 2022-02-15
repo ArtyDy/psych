a9af6b188a74c1505be80e298f60aff304c75d8a
ArtyDy <arthur.devemy@orange.fr>
2022-02-15 11:48:17 +0100

    Update rftest.py

--- rftest.py
+++ rftest.py
@@ -9,12 +9,14 @@ win = psychopy.visual.Window(
     size=[400, 400],
     units="pix",
     fullscr=True,
-    color=[0, 0, 0]
+    color="black"
 )
 
-rect=visual.Rect(win, width=50, height=50,fillColor=[0, 0, 0], lineWidth=5, lineColor=[1, 1, 1] )
+rect=visual.Rect(win, width=200, height=200,fillColor="black", lineWidth=5, lineColor=[1, 1, 1] )
 rect.draw()
+line=visual.Line(win, lineWidth=5, start =[0, -80], end=[0, 80], lineColor="white")
+line.draw()
 win.flip()
 core.wait(5)
 win.close()
-# %%
+  # %%

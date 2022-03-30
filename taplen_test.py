e1b03d05b6f70b520dab6d83db259e3b4a08a750
ArtyDy <arthur.devemy@orange.fr>
2022-03-30 16:44:02 +0200

    aaa

--- taplen_test.py
+++ taplen_test.py
@@ -11,7 +11,7 @@ from psychopy.hardware import keyboard
 
 #%% tap length
 #                                                                                                                                                          # %%
-sub='sub-07DB'
+sub='sub-28AP'
 
 kb=keyboard.Keyboard()
 conds=['600', '1200', '1000', '800', '800', '1200', '1200', '600', '800',
